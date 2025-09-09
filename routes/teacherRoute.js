502ff3b409278f47a1d11d962ec5d69c1713bf3d
Praneeth <praneethg807@gmail.com>
2025-09-09 21:12:51 +0530

    Update teacherRoute.js

--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -5,16 +5,12 @@ import { getTeacherClasses } from '../controllers/classController.js';
 
 const router = Router();
 
-// Register new teacher
 router.post('/register', RegisterTeacher);
 
-// Teacher login
 router.post('/login', LoginTeacher);
 
-// Get all classes (with students) for logged-in teacher
 router.get('/classes', verifyToken, getTeacherClasses);
 
-// Edit teacher
 router.put("/edit", verifyToken, editTeacher);
 
 router.get("/me", verifyToken, getTeacherDetails);
