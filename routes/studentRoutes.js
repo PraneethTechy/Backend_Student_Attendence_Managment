a7a316640abc1127cdb8657e2b3b819640fee058
Praneeth <praneethg807@gmail.com>
2025-09-09 21:12:30 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -4,16 +4,13 @@ import verifyToken from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-// Add a new student (and assign to class)
 router.post('/add', verifyToken, addStudent);
 
-// Delete student (and remove from class)
 router.delete('/delete/:studentId', verifyToken, deleteStudent);
 
-// Edit student
 router.put("/edit/:StudentId", verifyToken, editStudent);
 
-router.get('/my-students', verifyToken, getStudents); // ✅ new route
+router.get('/my-students', verifyToken, getStudents); 
 
 
 
