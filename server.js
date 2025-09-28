9dc1a6bab659783bd7a38e306fff82d90e2aa1df
Praneeth <praneethg807@gmail.com>
2025-09-28 06:46:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*", 
+origin: ["https://frontend-student-management-system-eight.vercel.app"] ,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
